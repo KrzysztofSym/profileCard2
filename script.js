8622f44ef04c3895ca8990f97d6789b66cd75135
KrzysztofSym <krzysztof.symoniuk@gmail.com>
2024-03-20 23:25:50 +0100

    update media queries

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const btn = document.querySelector('.more');
 const drop = document.querySelector('.dropdown-menu');
 
-
-
-// function btnFunction() {
-// btn.addEventListener('click', ()=> {
-//     drop.classList.toggle('active');
-// });
-// }
-
 function btnFunction() {
     btn.addEventListener('click', (event) => {
         event.stopPropagation(); // Prevent the body click event from being triggered
